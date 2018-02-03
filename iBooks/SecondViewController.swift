6a44e6fbb5242510612fb293d64dd68cdd4ec3bb
Soumen Ghosh <soumeng007@gmail.com>
2018-02-03 13:17:38 +0600

    Initial Commit

---
+++ iBooks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iBooks
+//
+//  Created by SOUMEN GHOSH on 3/2/18.
+//  Copyright © 2018 Sg106. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
